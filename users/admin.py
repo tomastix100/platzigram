b160907a31ef7760a75d329924f624109ace9c4f
Tomas Escobar <tomasj@unicauca.edu.co>
2021-01-26 11:51:14 -0500

    Dashboard de Administración

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,11 @@
 """User admin classes."""
 
 # Django
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
 
 # Models
+from django.contrib.auth.models import User
 from users.models import Profile
 
 
@@ -12,13 +14,9 @@ class ProfileAdmin(admin.ModelAdmin):
     """Profile admin."""
 
     list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
-    # Al darle click me lleva a la edicion
     list_display_links = ('pk', 'user',)
-    # No pueden haver los mismos valores en la lista anterior y en esta siguiente
-    # Me permite ditar ahi mismo el campo
     list_editable = ('phone_number', 'website', 'picture')
 
-    # Para poder buscar en la barra de busqueda
     search_fields = (
         'user__email',
         'user__username',
@@ -26,11 +24,58 @@ class ProfileAdmin(admin.ModelAdmin):
         'user__last_name',
         'phone_number'
     )
-    
-    # Colocar filtros para busqueda
+
     list_filter = (
         'user__is_active',
         'user__is_staff',
         'created',
         'modified',
     )
+    # Cuando colocamos los fields dentro de una sola tupla, estos se van a visualizar
+    # en columna, si los encerramos en dos, estos se veran horizontales
+    fieldsets = (
+        ('Profile', { # Este seria el tituo de la categoria
+            'fields': (('user', 'picture'),), 
+        }),
+        ('Extra info', { # Este seria el tituo de la categoria
+            'fields': (
+                ('website', 'phone_number'),
+                ('biography')
+            )
+        }),
+        ('Metadata', { # Este seria el tituo de la categoria
+            'fields': (('created', 'modified'),),
+        })
+    )
+    # Estas son variables que se van a poder ver en la categoria Metadata
+    # pero no se van apoder editar, si no colocaramos estas dentro de la 
+    # variable readonly_fields nos daria un error, ya que estamos diciendo 
+    # que se puede editar campos que son automaticps, cada que se crea un registro,
+    # por tal razon se deben bloquear su edición.
+
+    readonly_fields = ('created', 'modified',)
+
+class ProfileInline(admin.StackedInline):
+    """Profile in-line admin for users."""
+
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+
+class UserAdmin(BaseUserAdmin):
+    """Add profile admin to base user admin."""
+
+    inlines = (ProfileInline,)
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff'
+    )
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
